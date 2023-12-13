1d1502c65e3afac328d2715bee3a64053ab074fb
Dimushok1221 <dima.koshelev.2004@gmail.com>
2023-12-14 02:39:42 +0300

    Initial commit

---
+++ app/src/main/java/com/dimushok/lab32/AddRecAct.java
@@ -0,0 +1,45 @@
+package com.dimushok.lab32;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class AddRecAct extends AppCompatActivity {
+
+    DatabaseHandler databaseHandler = new DatabaseHandler(this);
+    EditText textAddRec;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_rec);
+
+        textAddRec = findViewById(R.id.textAddRec);
+    }
+
+    public void add_row(View v) {
+        String text = textAddRec.getText().toString();
+        databaseHandler.addStud(new Student(getLastId() + 1, text, DatabaseHandler.getDate()));
+    }
+
+    public int getLastId() {
+        String query = "SELECT MAX(" + Util.KEY_ID + ") FROM " + Util.TABLE_NAME + "; ";
+        SQLiteDatabase db = databaseHandler.getReadableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+
+        if (cursor != null && cursor.moveToFirst()) {
+            int lastInsertedId = cursor.getInt(0);
+            cursor.close();
+            return lastInsertedId;
+        }
+
+        db.close();
+        return 0;
+    }
+}
+
+
---
+++ app/src/main/java/com/dimushok/lab32/RecordsAct.java
@@ -0,0 +1,53 @@
+package com.dimushok.lab32;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class RecordsAct extends AppCompatActivity {
+
+    DatabaseHandler dbHandler = new DatabaseHandler(this);
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rec);
+
+        dbHandler= new DatabaseHandler(this);
+        textView = findViewById(R.id.textView);
+
+        addToTextView();
+    }
+
+    public void addToTextView(){
+        // Получаем доступ к базе данных для чтения
+        SQLiteDatabase db = dbHandler.getReadableDatabase();
+
+        // Выполняем запрос на получение всех записей из таблицы
+        Cursor cursor = db.rawQuery("SELECT * FROM " + Util.TABLE_NAME, null);
+
+        // Подготавливаем строку для отображения записей
+        StringBuilder recordsText = new StringBuilder();
+
+        // Перебираем результаты запроса и добавляем их к строке
+        while (cursor.moveToNext()) {
+            int id = cursor.getInt(cursor.getColumnIndex(Util.KEY_ID));
+            String name = cursor.getString(cursor.getColumnIndex(Util.KEY_NAME));
+            String addTime = cursor.getString(cursor.getColumnIndex(Util.KEY_ADDTIME));
+            // Здесь вам нужно использовать фактические имена столбцов в вашей таблице
+
+            recordsText.append("id: ").append(id).append(", name: ").append(name).append(", addTime: ").append(addTime).append("\n");
+        }
+
+        // Закрываем курсор и базу данных
+        cursor.close();
+        db.close();
+
+        // Отображаем записи в TextView
+        textView.setText(recordsText.toString());
+    }
+}
